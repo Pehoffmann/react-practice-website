fed3b1a451956ce36b3ba8b07601c3bba8207707
Pehoffmann <pedrob_hoffmann@outlook.com>
2021-03-08 22:09:28 -0300

    feature:aula1

---
+++ frontend/src/pages/Categoria/index.js
@@ -0,0 +1,13 @@
+import Main from '../../components/Main'
+import "./style.css";
+
+const Categoria = () => {
+  return (
+    <>
+      <small>Você está na página Categoria</small>
+      <Main />
+    </>
+  )
+};
+
+export default Categoria;
---
+++ frontend/src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import Main from '../../components/Main'
+import './style.css'
+
+const Home = () => {
+  return (
+    <>
+      <small>Você está na página Home</small>
+      <Main />
+    </>
+  )
+}
+
+export default Home
